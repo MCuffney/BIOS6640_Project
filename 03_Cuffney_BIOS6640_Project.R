8220b9610a4bc3ba50aeacc6d8f96e1e9d57f555
Unknown <michael.cuffney@gmail.com>
2017-12-07 10:20:08 -0700

    Secound stage
    cleaned-up reading in all the weather data

---
+++ 03_Cuffney_BIOS6640_Project.R
@@ -0,0 +1,8 @@
+##################################
+# BIOS 6640: Project             #
+# 001_Cuffney_BIOS6640_Project.R #
+# Exploratory analysis           #
+# Michael Cuffney                #
+# Data Created: 12/05/2017       #
+# Last Edited: 12/05/2017        #
+##################################
---
+++ 04_Cuffney_BIOS6640_Project.R
@@ -0,0 +1,8 @@
+##################################
+# BIOS 6640: Project             #
+# 002_Cuffney_BIOS6640_Project.R #
+# Final Possion analysis         #
+# Michael Cuffney                #
+# Data Created: 12/05/2017       #
+# Last Edited: 12/05/2017        #
+##################################
